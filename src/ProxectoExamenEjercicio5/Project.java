bebc621bd693cfc2cd3c84cca1a9d13c1d38cf0b
Roberto <pruebasegu987@gmail.com>
2022-05-03 19:41:14 +0200

    Proyecto de creacion de persona. Hecho

---
+++ src/ProxectoExamenEjercicio5/Project.java
@@ -0,0 +1,9 @@
+package ProxectoExamenEjercicio5;
+
+public class Project {
+	public static void main(String[] args) {
+        Persona p = new Persona("Pepe");
+        
+        System.out.println(p);
+    }
+}
